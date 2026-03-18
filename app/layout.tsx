ed06b376758401b090a3fd4a86f5dc1ed3210205
Aditya Negi <adityanegi2206@gmail.com>
2026-03-18 12:52:48 +0530

    speed

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Footer />
         <ScrollToTop />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
