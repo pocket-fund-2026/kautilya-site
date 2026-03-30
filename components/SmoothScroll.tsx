fd1b14e6165177add992da6c8df27ebd379832d3
Aditya Negi <adityanegi2206@gmail.com>
2026-03-30 16:44:17 +0530

    butter scroll mobile

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -8,6 +8,7 @@ export default function SmoothScroll() {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothTouch: true,
       touchMultiplier: 2,
     });
 
